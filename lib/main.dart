011f02714bde118b63c84ff818dbe9e943f26a1e
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-25 17:04:37 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/screens/wrapper.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Wrapper(), // wrapper is going to be the home screen
+    );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/screens/authenticate/authenticate.dart';
 import 'package:flutter_demo/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 // return either Home or Authenticate widget
-    return Home();
+    return Home(); // whatever you return here is shown on the home screen
   }
 }
