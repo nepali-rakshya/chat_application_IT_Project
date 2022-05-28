a58a4d757609ae784b55303fdacb4af6bf3f6f8a
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-28 18:35:31 +0530

    first commit

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -11,8 +11,22 @@ class AuthService {
 
   // create user obj based on FirebaseUser
 
-  MyUser? _userfromFirebase(User user) {
+  MyUser? _userFromFirebaseUser(User? user) {
     return user != null ? MyUser(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+
+/* Explanation of the user stream
+1. the function is return a stream of User
+2. 'get' keyword is to declare as a getter, if we say .user for any object, it will return the below syntax 
+3. authStateChanged is to notify about changes to the user's sign-in state
+4. method map() is to reassign for every FirebaseUser in _auth.authStateChange it will change to _userFromFirebaseUser which is our User*/
+
+  Stream<MyUser?> get user {
+    return _auth
+        .authStateChanges()
+        .map((User? user) => _userFromFirebaseUser(user));
   }
 
   // signInAnon is a method
@@ -22,7 +36,7 @@ class AuthService {
       UserCredential userCredential =
           await FirebaseAuth.instance.signInAnonymously();
       User? user = userCredential.user!;
-      return _userfromFirebase(user);
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
