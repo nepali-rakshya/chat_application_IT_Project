a183fb5af57497355d4ca0d1a76425e6e43fdbee
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-28 23:28:44 +0530

    first commit

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -20,20 +20,28 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-            child: const Text('Sign in anon'),
-            onPressed: () async {
-              // the variable is dynamic cuz it can be null or firebase user
-              dynamic result = await _auth
-                  .signInAnon(); // if you want to access the method signInAnon in auth.dart then use _auth object and with "." use the method.
-              if (result == null) {
-                print('error signing in');
-              } else {
-                print('signed in');
-                print(result);
-                //print(result.uid); this prints the user id
-              }
-            }),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0), //these are form field
+              TextFormField(onChanged: (val) {}),
+              SizedBox(height: 20.0),
+              TextFormField(
+                  obscureText: true, //for hiding the pwd
+                  onChanged: (val) {}),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text('Sign in'),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.pink[400]),
+                    textStyle: MaterialStateProperty.all(
+                        TextStyle(color: Colors.white))),
+                onPressed: () async {},
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
