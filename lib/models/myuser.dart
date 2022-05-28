6f8358322614a0a3ce4bd95baca7b8f8fc16d20c
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-28 17:36:03 +0530

    first commit

---
+++ lib/models/myuser.dart
@@ -0,0 +1,4 @@
+class MyUser {
+  final String? uid;
+  MyUser({this.uid}); //this is to get user id if the user is not null
+}
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_demo/models/myuser.dart';
 
 class AuthService {
   // Inside this class we are going to define all the different methods that are going to interact with firebase auth
@@ -7,14 +8,21 @@ class AuthService {
   // "_" means this (object) auth is private and can only be used in this file; "final" means it's not going to change in future
 
   // sign in anonymously
+
+  // create user obj based on FirebaseUser
+
+  MyUser? _userfromFirebase(User user) {
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
+
+  // signInAnon is a method
   Future signInAnon() async {
-    // signInAnon is a method
     try {
       // try and catch block
       UserCredential userCredential =
           await FirebaseAuth.instance.signInAnonymously();
-      User user = userCredential.user!;
-      return user;
+      User? user = userCredential.user!;
+      return _userfromFirebase(user!);
     } catch (e) {
       print(e.toString());
       return null;
