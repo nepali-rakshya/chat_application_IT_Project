b882d35163e4a503b04fb4cfa9fe787d2a9f1483
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-28 23:45:07 +0530

    first commit

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/services/auth.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+
+  //text field state
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: const Text('Sign up to Udham Nari'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0), //these are form field
+              TextFormField(onChanged: (val) {
+                setState(() => email =
+                    val); // whatever value we type in email section is updated into the email string
+              }),
+              SizedBox(height: 20.0),
+              TextFormField(
+                  obscureText: true, //for hiding the pwd
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text('Register'),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.pink[400]),
+                    textStyle: MaterialStateProperty.all(
+                        TextStyle(color: Colors.white))),
+                onPressed: () async {
+                  print(email);
+                  print(password);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
