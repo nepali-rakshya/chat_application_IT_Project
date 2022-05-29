262ca6d1e14103342a027835688706383387076c
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-29 21:47:56 +0530

    first commit

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -37,32 +37,39 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey, //associating the global form key with our form
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0), //these are form field
-              TextFormField(onChanged: (val) {
-                setState(() => email =
-                    val); // whatever value we type in email section is updated into the email string
-              }),
+              TextFormField(
+                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val) {
+                    setState(() => email =
+                        val); // whatever value we type in email section is updated into the email string
+                  }),
               SizedBox(height: 20.0),
               TextFormField(
                   obscureText: true, //for hiding the pwd
+                  validator: (val) =>
+                      val!.length < 6 ? 'Enter a password 6+ chars long' : null,
                   onChanged: (val) {
                     setState(() => password = val);
                   }),
               SizedBox(height: 20.0),
               ElevatedButton(
-                child: Text('Register'),
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.brown[400]),
-                    textStyle: MaterialStateProperty.all(
-                        TextStyle(color: Colors.white))),
-                onPressed: () async {
-                  print(email);
-                  print(password);
-                },
-              )
+                  child: Text('Register'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.brown[400]),
+                      textStyle: MaterialStateProperty.all(
+                          TextStyle(color: Colors.white))),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      // when this runs the validate check then it goes to each text field and checks their validity
+                      print(email);
+                      print(password);
+                    }
+                  })
             ],
           ),
         ),
