716002afb7b2047ddbc75573e78363d6a3c8b630
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-29 22:38:52 +0530

    fisrt commit

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -12,10 +12,12 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth =
       AuthService(); //it is the instance of AuthService class in auth.dart
+  final _formKey = GlobalKey<FormState>(); // form key which is global key
 
   //text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -38,32 +40,49 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey, //associating the global form key with our form
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0), //these are form field
-              TextFormField(onChanged: (val) {
-                setState(() => email =
-                    val); // whatever value we type in email section is updated into the email string
-              }),
+              TextFormField(
+                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val) {
+                    setState(() => email =
+                        val); // whatever value we type in email section is updated into the email string
+                  }),
               SizedBox(height: 20.0),
               TextFormField(
                   obscureText: true, //for hiding the pwd
+                  validator: (val) =>
+                      val!.length < 6 ? 'Enter a password 6+ chars long' : null,
                   onChanged: (val) {
                     setState(() => password = val);
                   }),
               SizedBox(height: 20.0),
               ElevatedButton(
-                child: Text('Sign in'),
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.brown[400]),
-                    textStyle: MaterialStateProperty.all(
-                        TextStyle(color: Colors.white))),
-                onPressed: () async {
-                  print(email);
-                  print(password);
-                },
-              )
+                  child: Text('Sign in'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.brown[400]),
+                      textStyle: MaterialStateProperty.all(
+                          TextStyle(color: Colors.white))),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      print('valid');
+                      // when this runs the validate check then it goes to each text field and checks their validity
+
+                      // if (result == null) {
+                      //   setState(() => error = 'please supply a valid email');
+                      // }
+                    }
+                  }),
+              SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              ),
             ],
           ),
         ),
