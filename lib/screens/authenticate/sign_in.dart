61c888381fd183420fcc1cfd043326406cc1eacb
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-30 12:17:57 +0530

    first commit

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/services/auth.dart';
 import 'package:flutter_demo/shared/constants.dart';
+import 'package:flutter_demo/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final toggleView;
@@ -14,6 +15,7 @@ class _SignInState extends State<SignIn> {
   final AuthService _auth =
       AuthService(); //it is the instance of AuthService class in auth.dart
   final _formKey = GlobalKey<FormState>(); // form key which is global key
+  bool loading = false;
 
   //text field state
   String email = '';
@@ -22,77 +24,85 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        backgroundColor: Colors.grey[400],
-        elevation: 0.0,
-        title: const Text('Sign in to Udham Nari'),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey, //associating the global form key with our form
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0), //these are form field
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
-                  onChanged: (val) {
-                    setState(() => email =
-                        val); // whatever value we type in email section is updated into the email string
-                  }),
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration:
-                      textInputDecoration.copyWith(hintText: 'Password'),
-                  obscureText: true, //for hiding the pwd
-                  validator: (val) =>
-                      val!.length < 6 ? 'Enter a password 6+ chars long' : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  }),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                  child: Text('Sign in'),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.brown[400]),
-                      textStyle: MaterialStateProperty.all(
-                          TextStyle(color: Colors.white))),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      dynamic result = await _auth.signInWithEmailAndPassword(
-                          email, password);
-                      // when this runs the validate check then it goes to each text field and checks their validity
+    return loading
+        ? Loading() // only when loading is true the loading widget is shown else scaffold is shown
+        : Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              backgroundColor: Colors.grey[400],
+              elevation: 0.0,
+              title: const Text('Sign in to Udham Nari'),
+              actions: <Widget>[
+                TextButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey, //associating the global form key with our form
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0), //these are form field
+                    TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val!.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email =
+                              val); // whatever value we type in email section is updated into the email string
+                        }),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        obscureText: true, //for hiding the pwd
+                        validator: (val) => val!.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        }),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                        child: Text('Sign in'),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.brown[400]),
+                            textStyle: MaterialStateProperty.all(
+                                TextStyle(color: Colors.white))),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .signInWithEmailAndPassword(email, password);
+                            // when this runs the validate check then it goes to each text field and checks their validity
 
-                      if (result == null) {
-                        setState(() =>
-                            error = 'could not sign in with those credentials');
-                      }
-                    }
-                  }),
-              SizedBox(
-                height: 12.0,
+                            if (result == null) {
+                              setState(() => error =
+                                  'could not sign in with those credentials');
+                              loading = false;
+                            }
+                          }
+                        }),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    ),
+                  ],
+                ),
               ),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
